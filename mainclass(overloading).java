b6f6fe1394cd7e57298103a70b2f97971a948eea
Abina-Anto <145189288+Abina-Anto@users.noreply.github.com>
2024-02-28 12:27:19 +0530

    Create mainclass(overloading).java

---
+++ mainclass(overloading).java
@@ -0,0 +1,18 @@
+class myclass{
+	int a;
+	double b;
+	void display(int a){
+		System.out.println("value of a:"+a);
+	}
+	void display(int a,double b){
+	System.out.println("value of a and b"+a+"and"+b);
+	}
+}
+public class mainclass{
+	public static void main(String args[]){
+	myclass obj=new myclass();
+	obj.display(100);
+	obj.display(100,108.4);
+	}
+	}
+	
