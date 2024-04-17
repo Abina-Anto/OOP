4d6dd0dd11cd801d3e85e90d09e4164c6ca8bda0
Abina-Anto <145189288+Abina-Anto@users.noreply.github.com>
2024-04-17 09:32:15 +0530

    Create NestedTry.java

---
+++ NestedTry.java
@@ -0,0 +1,30 @@
+class NestedTry{
+	public static void main(String args[]){
+	int number[]={20,40,60,80,100};
+	int denom[]={5,0,0,10};
+	try
+	{
+		for(int i=0;i<number.length;i++)
+		{
+			try
+			{
+				int q=number[i]/denom[i];
+				System.out.println(number[i]+"/"+denom[i]+"="+q);
+			}
+			catch(ArithmeticException e)
+			{
+				System.out.println("Divide by 0:"+e);
+			}
+			catch(ArrayIndexOutOfBoundsException e)
+			{
+				System.out.println("Array Index oob:"+e);
+				System.out.println("Program terminated");
+			}
+		}
+	}
+	finally
+	{
+		System.out.println("in final block");
+	}
+	}
+}
